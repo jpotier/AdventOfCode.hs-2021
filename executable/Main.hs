4dfaf8c9635994ad6d4640ca28ff37c8d40e5eda
Martin Potier <martin.potier@marvid.fr>
2022-01-21 18:50:21 +0200

    Day 2

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
 import qualified AoC.Day1  as Day1
+import qualified AoC.Day2  as Day2
 import qualified Data.Text as T
 
 main :: IO ()
 main = do
   input1 <- readFile "inputs/day1"
   putStrLn $ T.unpack $ Day1.output (T.pack input1)
+  input2 <- readFile "inputs/day2"
+  putStrLn $ T.unpack $ Day2.part1 (T.pack input2)
+  putStrLn $ T.unpack $ Day2.part2 (T.pack input2)
--- src/AoC/Day2.hs
+++ src/AoC/Day2.hs
@@ -2,7 +2,82 @@
 
 module AoC.Day2 where
 
-import           Data.Text
+import           Data.Text (Text)
+import           Data.Maybe (catMaybes)
+import           Text.Read (readMaybe)
+-- import           Debug.Trace
+import qualified Data.Text as T
 
-output :: Text
-output = "Day2"
+-- {{{ Part 1
+part1 :: Text -> Text
+part1 = T.pack . show . result1 . walk . parse
+
+data Position = Position
+  { depth      :: Int
+  , horizontal :: Int
+  }
+  deriving Show
+
+result1 :: Position -> Int
+result1 (Position d h) = d * h
+
+walk :: Ops -> Position
+walk ops = foldr step pos0 ops
+
+step :: Op -> Position -> Position
+step (OpForward n) (Position d h) = Position d (h+n)
+step (OpDown n)    (Position d h) = Position (d+n) h
+step (OpUp n)      (Position d h) = Position (d-n) h
+
+pos0 :: Position
+pos0 = Position 0 0
+
+data Op = OpForward  Int
+        | OpDown     Int
+        | OpUp       Int
+  deriving (Show)
+
+type Ops = [ Op ]
+
+parse :: Text -> Ops
+parse txt = catMaybes $ fmap toOps $ T.lines txt
+  where
+    toOps :: Text -> Maybe Op
+    toOps = toOps' . T.words
+
+    toOps' :: [Text] -> Maybe Op
+    toOps' ("forward":n:[]) = Just . OpForward =<< readMaybe (T.unpack n)
+    toOps' ("down":n:[]) = Just . OpDown =<< readMaybe (T.unpack n)
+    toOps' ("up":n:[]) = Just . OpUp =<< readMaybe (T.unpack n)
+    toOps' _ = Nothing
+
+testInput1 :: Text
+testInput1 = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2\n"
+-- }}}
+
+
+-- {{{ Part 2
+part2 :: Text -> Text
+part2 = T.pack . show . result2 . walk2 . parse
+
+data Position2 = Position2
+  { depth2      :: Int
+  , horizontal2 :: Int
+  , aim         :: Int
+  }
+  deriving Show
+
+result2 :: Position2 -> Int
+result2 (Position2 d h _a) = d * h
+
+walk2 :: Ops -> Position2
+walk2 ops = foldr step2 pos0' (reverse ops)
+
+pos0' :: Position2
+pos0' = Position2 0 0 0
+
+step2 :: Op -> Position2 -> Position2
+step2 (OpForward n) (Position2 d h a) = Position2 (d+a*n) (h+n)  a
+step2 (OpDown n)    (Position2 d h a) = Position2  d       h    (a+n)
+step2 (OpUp n)      (Position2 d h a) = Position2  d       h    (a-n)
+-- }}}
