50713a0ba75513a6ae2b3173b126eb20e3d4ff98
Martin Potier <martin.potier@marvid.fr>
2022-01-21 17:00:23 +0200

    Fix the flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,45 @@
+# Seeded from: https://github.com/MasseR/ics-print/blob/main/flake.nix
 {
-  # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
-  description = "A flake for adventofcode";
-  inputs.nixpkgs.url = "nixpkgs";
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      });
-    in
-    {
-      overlay = (final: prev: {
-        adventofcode = final.haskellPackages.callCabal2nix "adventofcode" ./. {};
-      });
-      packages = forAllSystems (system: {
-         adventofcode = nixpkgsFor.${system}.adventofcode;
-      });
-      defaultPackage = forAllSystems (system: self.packages.${system}.adventofcode);
-      checks = self.packages;
-      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
-        in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.adventofcode];
-          withHoogle = true;
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            ghcid
-            cabal-install
-          ];
-        # Change the prompt to show that you are in a devShell
-        shellHook = "hpack";
-        });
+  description = "AdventOfCode 2021";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    easy-hls = {
+      url = "github:jkachmar/easy-hls-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs = { self, nixpkgs, flake-utils, easy-hls }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] ( system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hp = pkgs.haskellPackages.extend (self: super: {
+          adventofcode = self.callCabal2nix "adventofcode" ./. {};
+        });
+        hls = easy-hls.defaultPackage.${system};
+      in
+      rec {
+
+        packages = { inherit (hp) adventofcode; };
+
+        defaultPackage = packages.adventofcode;
+
+        apps.adventofcode = flake-utils.lib.mkApp { drv = packages.adventofcode; };
+        defaultApp = apps.adventofcode;
+
+        devShell = hp.shellFor {
+          packages = h: [h.adventofcode];
+          withHoogle = true;
+          buildInputs = with pkgs; [
+            entr
+            cabal-install
+            hp.hlint
+            stylish-haskell
+            ghcid
+            hls
+          ];
+        };
+      }
+    );
 }
